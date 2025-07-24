989a7162f8a4ee71088d897c440ea3bd5da44d4e
gleisefabiane <153039523+gleisefabiane@users.noreply.github.com>
2025-07-24 11:51:19 -0300

    web-ok

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+//declaro os objetos - lâmpadas
+
+let lampadaSalaLigada = false
+let lampadaCozinhaLigada = false
+
 
 function ligarSala(){
-    alert ('ligarLampadaSala')
+    if (lampadaSalaLigada == false){
+        document.getElementById("lamp-sala").classList.add("acesa")
+        document.getElementById("bt-sala").textContent = "Desligar"
+        lampadaSalaLigada = true
+    }
+    else{
+        document.getElementById("lamp-sala").classList.remove("acesa")
+        document.getElementById("bt-sala").textContent = "Ligar"
+        lampadaSalaLigada = false
+    }
 }
 
-function ligarCozinha(){
-    alert ('ligarLampadaCozinha')
+function ligarCozinha()
+{
+    if (lampadaCozinhaLigada == false){
+        lampadaCozinhaLigada = true
+        document.getElementById("bt-cozinha").textContent = "Desligar"
+    }
+    else
+    {
+        lampadaCozinhaLigada = false
+        document.getElementById("bt-cozinha").textContent = "Ligar"
+    }
+    document.getElementById("lamp-cozinha").classList.toggle("acesa")
+ 
 }
